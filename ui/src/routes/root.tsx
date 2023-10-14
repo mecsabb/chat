bed1dd25e287579df156220b0bb9716a8dc51e85
Mitchell <sabbadinimitchell@gmail.com>
2023-10-13 22:13:22 -0400

    [FE] Added navbar component

--- ui/src/routes/root.tsx
+++ ui/src/routes/root.tsx
@@ -1,19 +1,11 @@
 //TODO fix image serving 
 
+import Navbar from "../components/Navbar"
+
 export default function Home() {
     return (
         <>
-        <header>
-            <nav className="dark:bg-gray-800 border-gray-700 px-4 py-2.5">
-                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                    <span className="text-xl font-extrabold px-5 text-white">Chat</span>
-                    <div className="flex items-center lg:order-2">
-                        <a href="/login" className="text-white px-10">Log in</a>
-                        <a href="/#" className="text-white px-10">Get started</a>
-                    </div>
-                </div>
-            </nav>
-        </header>
+        <Navbar/>
         <body className="h-screen bg-gray-400"> 
             <div className="relative flex flex-col items-center justify-center px-6 py-8 mx-auto top-1/3 w-1/3">
                 <img src='../assets/speech-bubble-1078.png' className=""></img>
